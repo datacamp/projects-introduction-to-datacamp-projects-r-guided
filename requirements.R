b6e8b8f2e4c16887f6b9de424a3408f82eb57eed
Tim Sangster <49642426+TimSangster@users.noreply.github.com>
2022-03-15 16:57:36 +0100

    unpin dependencies

--- requirements.R
+++ requirements.R
@@ -1,3 +1,3 @@
 # Installing required packages
-devtools::install_version('ggplot2', version = '3.2.1')
-devtools::install_version('forecast', version = '8.9')
+devtools::install_version('ggplot2')
+devtools::install_version('forecast')
